feaa70c9fb320b9071856d78d93b0c912b43ccc2
rosisimbaui-commits <rosi.simba.ui@phinmaed.com>
2026-03-19 08:16:32 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+// Load environment variables (simple example using dotenv style)
+return [
+    'db_host' => 'localhost',
+    'db_name' => 'my_app',
+    'db_user' => 'root',
+    'db_pass' => 'password',
+    'salt'   => 'random_salt_123'
+];
