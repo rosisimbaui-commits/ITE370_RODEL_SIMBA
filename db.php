5debb2ad40f5198105a2384fa3cc227d6dfa9a0c
rosisimbaui-commits <rosi.simba.ui@phinmaed.com>
2026-03-19 08:17:20 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+// db.php
+$config = require 'config.php';
+
+try {
+    $dsn = "mysql:host={$config['db_host']};dbname={$config['db_name']};charset=utf8mb4";
+    $pdo = new PDO($dsn, $config['db_user'], $config['db_pass'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Error handling
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    // Error Handling
+    die("Database connection failed: " . $e->getMessage());
+}
